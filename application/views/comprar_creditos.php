279ecc4a12742866476947cfb2df0e99e1454b9a
briandacebreros <briandacebreros@gmail.com>
2017-02-03 10:07:38 +0000

    Paypal
    Se agregó Paypal creando el boton desde el sitio de Paypal y se ajustó a
    las necesidades.
    El boton sirve, sólo falta agregarle diseño

--- application/views/comprar_creditos.php
+++ application/views/comprar_creditos.php
@@ -15,24 +15,22 @@
             <div class="col-md-4 col-md-offset-4">
     
                 <h4 class="citas-title">COMPRAR CREDITOS</h4>
-                
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-
-                  <!-- Saved buttons use the "secure click" command -->
-                  <input type="hidden" name="cmd" value="_s-xclick">
-
-                  <!-- Saved buttons are identified by their button IDs -->
-                  <input type="hidden" name="hosted_button_id" value="221">
-
-                  <!-- Saved buttons display an appropriate button image. -->
-                  <input type="image" name="submit"
-                    src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif"
-                    alt="PayPal - The safer, easier way to pay online">
-                  <img alt="" width="1" height="1"
-                    src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" >
-
-                </form>
-
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+					<input type="hidden" name="cmd" value="_s-xclick">
+					<input type="hidden" name="hosted_button_id" value="WFAVG394DB3MA">
+					<table>
+					<tr><td>
+					<input type="hidden" name="on0" value="Numero de citas">Numero de citas</td></tr><tr><td>
+					<input name="os0" id="os0" type="hidden" value="1 Cita">
+					<h2>Seleccionar numero de citas</h2>
+					<div id="cita1" class="btn-selecc-creditos">1 Cita $500</div>
+					<div id="cita4" class="btn-selecc-creditos">4 Citas $1,200</div>
+					</td></tr>
+					</table>
+					<input type="hidden" name="currency_code" value="MXN">
+					<button border="0" name="submit">PAGAR</button>
+					
+				</form>
 
 
                
@@ -43,3 +41,33 @@
     
     
 </div>
+<script>
+$('.btn-selecc-creditos').click(function() {
+	var clave = this.id;
+	if( clave == 'cita1' ) {
+		$('#os0').prop('value', '1 Cita');
+		$('#cita4').css('background-color','#ccc');
+	}
+	if( clave == 'cita4' ) {
+		$('#os0').prop('value', '4 Citas');
+		$('#cita1').css('background-color','#ccc');
+	}
+	$(this).css('background-color','#439aca');
+	
+});
+</script>
+<style>
+.btn-selecc-creditos {
+	position: relative;
+	display: inline-block;
+	margin-right: 10px;
+	width: 100px;
+	height: 100px;
+	background-color: #ccc;
+	cursor: pointer;
+}
+#cita1 {
+	background-color: #439aca;
+}
+
+</style>
